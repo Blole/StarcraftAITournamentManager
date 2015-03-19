d81dbdea9de0360c40a40e6b9fed811049b25e67
Björn Holm <bjh@kth.se>
2015-03-19 16:45:26 +0100

    run_client.bat now requests admin; hid consoles from run_(server|client).bat
    run_client.bat now requests administrator privileges because it wouldn't have
    permission to kill starcraft and chaoslauncher otherwise. run_server.bat also
    works if it's started with administator privileges, but it's not neccessary.

--- client/run_client.bat
+++ client/run_client.bat
@@ -1,1 +1,30 @@
-java -jar client.jar client_settings.ini
+@echo off
+setlocal enableextensions
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    cd /D "%~dp0"
+:--------------------------------------
+
+start javaw -jar client.jar client_settings.ini
+exit
--- server/run_server.bat
+++ server/run_server.bat
@@ -1,1 +1,6 @@
-java -jar server.jar server_settings.ini
+@echo off
+setlocal enableextensions
+
+cd /D "%~dp0"
+start javaw -jar server.jar server_settings.ini
+exit
